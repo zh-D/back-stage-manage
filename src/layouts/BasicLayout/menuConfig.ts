af3de6520a0f28f48745677d4f4459577d9c9ac5
zh-D <1641245614@qq.com>
2021-05-22 21:26:58 +0800

    add all routes

--- src/layouts/BasicLayout/menuConfig.ts
+++ src/layouts/BasicLayout/menuConfig.ts
@@ -18,28 +18,78 @@ const asideMenuConfig = [
   },
   {
     name: '房屋维修管理',
-    path: '/houserepair',
+    path: '/',
     icon: 'copy',
+    children: [
+      {
+        name: '维修信息管理',
+        path: '/repaire/repaireinfo',
+      },
+      {
+        name: '添加维修信息',
+        path: '/repaire/addrepaireinfo',
+      },
+    ]
   },
   {
     name: '绿化工作',
-    path: '/greening',
+    path: '/',
     icon: 'chart-bar',
+    children: [
+      {
+        name: '绿化信息管理',
+        path: '/greening/greeninginfo',
+      },
+      {
+        name: '添加绿化信息',
+        path: '/greening/addgreeninginfo',
+      },
+    ]
   },
   {
     name: '安保工作',
-    path: '/securityguard',
+    path: '/',
     icon: 'calendar',
+    children: [
+      {
+        name: '安保信息管理',
+        path: '/guard/guardinfo',
+      },
+      {
+        name: '添加安保信息',
+        path: '/guard/addguardinfo',
+      },
+    ]
   },
   {
     name: '清洁工作',
-    path: '/cleaning',
+    path: '/',
     icon: 'warning',
+    children: [
+      {
+        name: '清洁信息管理',
+        path: '/cleaning/cleaninginfo',
+      },
+      {
+        name: '添加清洁信息',
+        path: '/cleaning/addcleaninginfo',
+      },
+    ]
   },
   {
     name: '收费管理',
-    path: '/paymanagement',
+    path: '/pay',
     icon: 'set',
+    children: [
+      {
+        name: '收费信息管理',
+        path: '/paymanagement/payinfo',
+      },
+      {
+        name: '添加收费信息',
+        path: '/paymanagement/addpayinfo',
+      },
+    ]
   },
   {
     name: '登录&注册',
--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,19 @@ import UserLayout from '@/layouts/UserLayout';
 import Login from '@/pages/Login';
 import Register from '@/pages/Register';
 import BasicLayout from '@/layouts/BasicLayout';
-import HouseInfo from '@/pages/House/EditHouseInfo';
+import EditHouseInfo from '@/pages/House/EditHouseInfo';
 import AddHouseInfo from '@/pages/House/AddHouseInfo';
+import EditRepaireInfo from '@/pages/Repaire/EditRepaireInfo';
+import AddRepaireInfo from '@/pages/Repaire/AddRepaireInfo';
+import EditGreeningInfo from '@/pages/Greening/EditGreeningInfo';
+import AddGreeningInfo from '@/pages/Greening/AddGreeningInfo';
+import EditGuardInfo from '@/pages/Guard/EditGuardInfo';
+import AddGuardInfo from '@/pages/Guard/AddGuardInfo';
+import EditCleaningInfo from '@/pages/Cleaning/EditCleaningInfo';
+import AddCleaningInfo from '@/pages/Cleaning/AddCleaningInfo';
+import EditPayInfo from '@/pages/Pay/EditPayInfo';
+import AddPayInfo from '@/pages/Pay/AddPayInfo';
+
 
 const routerConfig: IRouterConfig[] = [
   {
@@ -13,11 +24,51 @@ const routerConfig: IRouterConfig[] = [
     children: [
       {
         path: '/house/houseinfo',
-        component: HouseInfo,
+        component: EditHouseInfo,
       },
       {
         path: '/house/addhouseinfo',
         component: AddHouseInfo,
+      },
+      {
+        path: '/repaire/repaireinfo',
+        component: EditRepaireInfo,
+      },
+      {
+        path: '/repaire/addrepaireinfo',
+        component: AddRepaireInfo,
+      },
+      {
+        path: '/greening/greeninginfo',
+        component: EditGreeningInfo,
+      },
+      {
+        path: '/greening/addgreeninginfo',
+        component: AddGreeningInfo,
+      },
+      {
+        path: '/guard/guardinfo',
+        component: EditGuardInfo,
+      },
+      {
+        path: '/guard/addguardinfo',
+        component: AddGuardInfo,
+      },
+      {
+        path: '/cleaning/cleaninginfo',
+        component: EditCleaningInfo,
+      },
+      {
+        path: '/cleaning/addcleaninginfo',
+        component: AddCleaningInfo,
+      },
+      {
+        path: '/paymanagement/payinfo',
+        component: EditPayInfo,
+      },
+      {
+        path: '/paymanagement/addpayinfo',
+        component: AddPayInfo,
       },
       {
         path: '/',
